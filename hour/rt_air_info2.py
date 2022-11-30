b0d3c17e0fd84a00803ef147d5ad0bf60af06683
ksee1230 <ksee1230@gmail.com>
2022-11-30 16:16:39 +0900

    함수 변경 및 코드 수정

--- hour/rt_air_info2.py
+++ hour/rt_air_info2.py
@@ -7,9 +7,9 @@ import os
 import sys
 
 # url 입력
-
 url = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=9EVeUgs2qfDAndxMrmsipei8IlVyfYJLrYhjRHc1P3O1vpnEcS%2BX7CJByyCd81%2FdfwKZ1efWvF1WSaDwYG6ApA%3D%3D&returnType=json&numOfRows=1000&pageNo=1&sidoName=%EC%A0%84%EA%B5%AD&ver=1.2'
 
+# api 호출
 count = 0
 while 1:
     try:
@@ -34,104 +34,20 @@ json_ob = json.loads(contents)
 # 필요한 내용만 꺼내기
 body = json_ob['response']['body']['items']
 
-
-def calStateInteger(value, type):
+def get_state(value, flag):
     if value < 0:
         return "점검중"
+    standard = {"khai": [50, 100, 250], "pm10K": [30, 80, 150], "pm10W": [30, 50, 100], "pm25K": [15, 35, 75], "pm25W": [15, 25, 50], 
+                "o3": [0.03, 0.09, 0.15], "co": [2.0, 9.0, 15.0], "no2": [0.03, 0.06, 0.2], "so2": [0.02, 0.05, 0.15]}
 
-    if type == "khai":
-        if value <= 50:
-            return "좋음"
-        elif value <= 100:
-            return "보통"
-        elif value <= 250:
-            return "나쁨"
-        else:
-            return "최악"
-    elif type == "pm10K":
-        if value <= 30:
-            return "좋음"
-        elif value <= 80:
-            return "보통"
-        elif value <= 150:
-            return "나쁨"
-        else:
-            return "최악"
-    elif type == "pm10W":
-        if value <= 30:
-            return "좋음"
-        elif value <= 50:
-            return "보통"
-        elif value <= 100:
-            return "나쁨"
-        else:
-            return "최악"
-    elif type == "pm25K":
-        if value <= 15:
-            return "좋음"
-        elif value <= 35:
-            return "보통"
-        elif value <= 75:
-            return "나쁨"
-        else:
-            return "최악"
-    elif type == "pm25W":
-        if value <= 15:
-            return "좋음"
-        elif value <= 25:
-            return "보통"
-        elif value <= 50:
-            return "나쁨"
-        else:
-            return "최악"
+    if value <= standard[flag][0]:
+        return "좋음"
+    elif value <= standard[flag][1]:
+        return "보통"
+    elif value <= standard[flag][2]:
+        return "나쁨"
     else:
-        return ""
-
-
-def calStateFloat(value, type):
-
-    if value < 0:
-        return "점검중"
-
-    if type == "o3":
-        if value <= 0.03:
-            return "좋음"
-        elif value <= 0.09:
-            return "보통"
-        elif value <= 0.15:
-            return "나쁨"
-        else:
-            return "최악"
-    elif type == "co":
-        if value <= 2.0:
-            return "좋음"
-        elif value <= 9.0:
-            return "보통"
-        elif value <= 15.0:
-            return "나쁨"
-        else:
-            return "최악"
-    elif type == "no2":
-        if value <= 0.03:
-            return "좋음"
-        elif value <= 0.06:
-            return "보통"
-        elif value <= 0.2:
-            return "나쁨"
-        else:
-            return "최악"
-    elif type == "so2":
-        if value <= 0.02:
-            return "좋음"
-        elif value <= 0.05:
-            return "보통"
-        elif value <= 0.15:
-            return "나쁨"
-        else:
-            return "최악"
-    else:
-        return ""
-
+        return "최악"
 
 # 환경변수 불러오기
 host = os.environ.get('DB_HOST')  # ex) '127.0.0.1'
@@ -139,7 +55,6 @@ port = int(os.environ.get('DB_PORT'))  # ex) 3306
 user = os.environ.get('DB_USER')  # ex) 'root'
 password = os.environ.get('DB_PW')  # ex) '1234'
 database = os.environ.get('DB_NAME')  # ex) 'ApiExplorer'
-
 
 if (len(body) != 0):
     # connection 정보. 접속
@@ -163,33 +78,33 @@ if (len(body) != 0):
         station_name = a['stationName']
         sido_name = a['sidoName']
         data_time = a['dataTime']
-        so2_value = a['so2Value'] if (
-            (a['so2Flag'] is None) and (a['so2Value'] is not None)) else -1
-        so2_state = calStateFloat(float(so2_value), 'so2')
-        co_value = a['coValue'] if ((a['coFlag'] is None) and (
-            a['coValue'] is not None)) else -1
-        co_state = calStateFloat(float(co_value), 'co')
-        pm10_value = a['pm10Value'] if (
-            (a['pm10Flag'] is None) and (a['pm10Value'] is not None)) else -1
-        pm10_stateK = calStateInteger(int(pm10_value), 'pm10K')
-        pm10_stateW = calStateInteger(int(pm10_value), 'pm10W')
-        pm25_value = a['pm25Value'] if (
-            (a['pm25Flag'] is None) and (a['pm25Value'] is not None)) else -1
-        pm25_stateK = calStateInteger(int(pm25_value), 'pm25K')
-        pm25_stateW = calStateInteger(int(pm25_value), 'pm25W')
-        no2_value = a['no2Value'] if (
-            (a['no2Flag'] is None) and (a['no2Value'] is not None)) else -1
-        no2_state = calStateFloat(float(no2_value), 'no2')
-        o3_value = a['o3Value'] if ((a['o3Flag'] is None) and (
-            a['o3Value'] is not None)) else -1
-        o3_state = calStateFloat(float(o3_value), 'o3')
-        khai_value = a['khaiValue'] if (
-            a['khaiValue'] != '-') and (a['khaiValue'] is not None) else -1
-        khai_state = calStateInteger(int(khai_value), 'khai')
+        so2_value = float(a['so2Value']) if (
+                    (a['so2Flag'] is None) and (a['so2Value'] is not None)) else -1
+        so2_state = get_state(so2_value, 'so2')
+        co_value = float(a['coValue']) if (
+                    (a['coFlag'] is None) and (a['coValue'] is not None)) else -1
+        co_state = get_state(co_value, 'co')
+        pm10_value = int(a['pm10Value']) if (
+                    (a['pm10Flag'] is None) and (a['pm10Value'] is not None)) else -1
+        pm10_stateK = get_state(pm10_value, 'pm10K')
+        pm10_stateW = get_state(pm10_value, 'pm10W')
+        pm25_value = int(a['pm25Value']) if (
+                    (a['pm25Flag'] is None) and (a['pm25Value'] is not None)) else -1
+        pm25_stateK = get_state(pm25_value, 'pm25K')
+        pm25_stateW = get_state(pm25_value, 'pm25W')
+        no2_value = float(a['no2Value']) if (
+                    (a['no2Flag'] is None) and (a['no2Value'] is not None)) else -1
+        no2_state = get_state(no2_value, 'no2')
+        o3_value = float(a['o3Value']) if (
+                    (a['o3Flag'] is None) and (a['o3Value'] is not None)) else -1
+        o3_state = get_state(o3_value, 'o3')
+        khai_value = int(a['khaiValue']) if (
+                    a['khaiValue'] != '-') and (a['khaiValue'] is not None) else -1
+        khai_state = get_state(khai_value, 'khai')
 
         sql = "INSERT INTO rt_air_info(so2_value, so2_state, pm10_value, co_state, pm25_value, pm10_stateK, pm10_stateW, pm25_stateK, pm25_stateW, mang_name, no2_state, o3_state, khai_state, station_name, no2_value, khai_value, co_value, sido_name, data_time, o3_value) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        val = (float(so2_value), str(so2_state), int(pm10_value), str(co_state), int(pm25_value), str(pm10_stateK), str(pm10_stateW), str(pm25_stateK), str(pm25_stateW), str(mang_name), str(no2_state), str(o3_state), str(khai_state), str(station_name), float(
-            no2_value), int(khai_value), float(co_value), str(sido_name), str(data_time), float(o3_value))
+        val = (so2_value, so2_state, pm10_value, co_state, pm25_value, pm10_stateK, pm10_stateW, pm25_stateK, pm25_stateW, mang_name, no2_state, o3_state, khai_state, station_name, 
+            no2_value, khai_value, co_value, sido_name, data_time, o3_value)
         curs.execute(sql, val)
 
     curs.close()
